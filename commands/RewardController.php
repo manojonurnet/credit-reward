f9c3cb174cefe6600e91dc11f0ec1f0e01e54ee3
Manoj Neupane <manoj@proshore.eu>
2021-06-20 03:42:54 +0545

    Console command to calculate reward

---
+++ commands/RewardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+use app\models\Reward;
+use app\models\Customer;
+
+/**
+ * This command calculates customer reward points
+ */
+class RewardController extends Controller
+{
+    /**
+     * This command echoes what you have entered as the message.
+     * @param string $message the message to be echoed.
+     * @return int Exit code
+     */
+    public function actionIndex()
+    {
+        $customers = Customer::find()->all();
+        
+        $rewards = 0;
+        foreach ($customers as $customer) {
+            foreach ($customer->rewards as $reward) {
+                if ($reward->expiry_date <= date('Y-m-d')) {
+                    $reward->status = 0;
+                    $reward->save();
+                } else {
+                    $rewards += $reward->points;
+                }
+            }
+
+            $customer->reward = $rewards;
+            $customer->save();
+        }
+
+        print 'Rewards updated.' . PHP_EOL;
+        return ExitCode::OK;
+    }
+}
